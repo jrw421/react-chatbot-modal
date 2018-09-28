983fe2df10efc30098f7b5707c1563f45b20d620
jon <jonathan.brandwein@gmail.com>
2018-09-28 15:24:31 -0400

    intiial

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'example/main.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'example'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'example'),
+    host: '0.0.0.0',
+    disableHostCheck: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+};
